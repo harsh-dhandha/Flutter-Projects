409b30c16fb82e3caf4ea8641fba8f9dca5e391e
harsh_dhandha <147801074+harsh-dhandha@users.noreply.github.com>
2025-05-13 23:05:51 +0530

    Delete Dart programs directory

--- Dart programs/firstprogram/bin/firstprogram.dart
+++
@@ -1,5 +0,0 @@
-import 'package:firstprogram/firstprogram.dart' as firstprogram;
-
-void main(List<String> arguments) {
-  print('Hello world: ${firstprogram.calculate()}!');
-}
--- Dart programs/firstprogram/test/firstprogram_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:firstprogram/firstprogram.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
